a6f1a69a5928c5c00e7cd2d308bc0a3f12b99c5f
Wojciech Zając <wojciechzajaaac@gmail.com>
2019-09-12 23:10:13 +0200

    Send emails on a schedule

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
 const cron = require("node-cron")
 const express = require("express")
+const nodemailer = require("nodemailer")
+const {SENDER_EMAIL} = process.env
+const {SENDER_PASS} = process.env
 
 app = express()
 
-cron.schedule("* * * * *", function() {
-    console.log("running a task every minute " + new Date() );
-});
+let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: SENDER_EMAIL,
+        pass: SENDER_PASS
+    }
+})
 
-app.listen(3128);
+let mailOptions = {
+    from: SENDER_EMAIL,
+    to: "youremail@gmail.com, youremail+2@gmail.com",
+    cc: "youremail+cc@gmail.com",
+    subject: "Uszanowanie od Janosika",
+    html: `<h1>Welcome</h1>
+           <p>Treść emaila</p>
+           <p>Dalsza część</p>`
+}
 
+cron.schedule("*/1 * * * *", () => {
+    console.log("Running Cron Job " + new Date())
+    sendEmail()
+})
 
+sendEmail = () => {
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            throw error
+        } else {
+            console.log(`
+            Email successfully sent at ${new Date()}
+            Info: ${info.response}
+            `)
+        }
+    })
+}
+
+app.listen("3128")
